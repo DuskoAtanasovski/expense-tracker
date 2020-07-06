683ac6e4ffd4e6b4dda28647f35e19b8232cf7cf
DuskoAtanasovski <atanasovski95@yahoo.com>
2020-07-06 20:57:36 +0200

    First Commit

---
+++ src/main/java/com/expense/tracker/model/Category.java
@@ -0,0 +1,27 @@
+package com.expense.tracker.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name = "category")
+public class Category {
+
+	@Id
+	private Long id;
+
+	// Travel, Grocery, ...
+	private String name;
+
+	@ManyToOne(cascade = CascadeType.PERSIST)
+	private User user;
+
+}
