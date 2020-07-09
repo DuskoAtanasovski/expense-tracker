f5ffac42d0084629a07f16f7dca6e12ba087ad98
DuskoAtanasovski <atanasovski95@yahoo.com>
2020-07-09 15:47:26 +0200

    adding files

--- app/src/Expenses.js
+++ app/src/Expenses.js
@@ -3,38 +3,86 @@ import AppNav from './AppNav';
 import DatePicker from 'react-datepicker';
 import './App.css';
 import "react-datepicker/dist/react-datepicker.css";
-import { Container, FormGroup, Button, Form, Input, Label } from 'reactstrap';
+import {Table, Container, FormGroup, Button, Form, Input, Label } from 'reactstrap';
 import {Link} from 'react-router-dom';
+import Moment from 'react-moment';
+
 
 class Expenses extends Component {
-    state = {
-        date : new Date(),
-        isLoading : true,
-        expenses : [],
-        Categories : []
-      }
+
+    // {
+    //     "id": 100,
+    //     "expensedate": "2020-07-02T19:00:00Z",
+    //     "description": "New York Bussines Trip",
+    //     "location": "New York",
+    //     "category": {
+    //         "id": 1,
+    //         "name": "Travel"
+    //     }
+    // }
+
+    emptyItem = {
+        id : '103',
+        expensedate : new Date(),
+        description : '',
+        location : '',
+        categories : [1 , 'Travel']
+    }
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            isLoading : false,
+            Categories : [],
+            Expenses : [],
+            date : new Date(),
+            item : this.emptyItem
+          }
+    }
+
+    
 
       async componentDidMount() {
+        
           const response= await fetch('/api/categories');
           const body= await response.json();
-          this.setState({Categories : body , isLoading : false})
+          this.setState({Categories : body , isLoading : false});
+
+          const responseExp= await fetch('/api/expenses');
+          const bodyExp= await responseExp.json();
+          this.setState({Expenses : bodyExp , isLoading : false});
       }
 
 
     render() { 
         const title= <h3>Add Expense</h3>
-        const {Categories, isLoading}=this.state;
+        const {Categories}=this.state;
+        const {Expenses, isLoading}=this.state;
 
         if(isLoading)
             return(<div>Loading...</div>)
 
-     let optionList   =  
+        let optionList   =  
                 Categories.map( category =>
-                    <option id={category.id}>
+                    <option value={category.id} key={category.id}>
                         {category.name}
                     </option>
                     )
             
+        let rows =
+            Expenses.map( expense =>
+                <tr>
+                    <td>{expense.description}</td>
+                    <td>{expense.location}</td>
+                    <td>{expense.expensedate}</td>
+                    <td>{expense.category.name}</td>
+                    <td><Button size="sm" color="danger" onClick={() => this.remove(expense.id)}>Delete</Button></td>
+
+
+                </tr>
+                
+                )
 
         return ( 
             <div>
@@ -54,8 +102,6 @@ class Expenses extends Component {
                             {optionList}
                         </select>
                        
-
-                        <Input type="text" name="category" id="category" onChange={this.handleChange}/>
                     </FormGroup>
 
                     <FormGroup>
@@ -75,10 +121,29 @@ class Expenses extends Component {
                         <Button color="secondary" tag={Link} to="/">Cancel</Button>
                     </FormGroup>
                 </Form>
-
-
                 </Container>
+
+                {''}   
+                <Container>
+                    <h3>Expense List</h3>
+                    <Table className="mt-4">
+                        <thead>
+                            <tr>
+                                <th width="30%">Description</th>
+                                <th width="10%">Location</th>
+                                <th>Date</th>
+                                <th>Category</th>
+                                <th width="10%">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {rows}
+                        </tbody>
+                    </Table>
+                </Container>
+                
             </div> 
+
             );
     }
 }
